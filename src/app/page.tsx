d2ae02de919386eead0b741a029fb98dfebac1d3
Artur Lipin <cookiess718@gmail.com>
2024-11-01 15:35:43 +0200

    shadcn install fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Next.js",
 };
-const apiUrl = "/api/show-data";
+// const apiUrl = "/api/show-data";
 export default async function Home() {
   //useeffect with fetch
   return (
-    <div>
+    <div className="flex flex-col h-screen w-full items-center justify-center px-4">
       <main>Main page</main>
+      <Button size={"lg"} variant={"destructive"}>
+        Button
+      </Button>
     </div>
   );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -9,7 +9,7 @@ const UserList: React.FC = () => {
     fetch("/api/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
-  }, []);
+  }, [users]);
   return <></>;
 };
 
