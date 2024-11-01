bc7b1fb4ea108742efa2400ca71315096d934c37
Artur Lipin <cookiess718@gmail.com>
2024-11-01 14:45:41 +0200

    shadcn install

---
+++ src/components/UserList.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const UserList: React.FC = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return <></>;
+};
+
+export default UserList;
